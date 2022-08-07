6b5622b05f9a68be67857d0d967baeeb887944e5
UtkarshDixit-py <utkarshdixit303@gmail.com>
2022-08-07 21:27:04 +0530

    Form

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
-import Head from 'next/head'
-import Table from '../components/Table';
+import Head from "next/head";
+import Table from "../components/Table";
+import Form from "../components/Form";
 
-import { BiUserPlus } from 'react-icons/bi';
+import { BiUserPlus } from "react-icons/bi";
 
 export default function Home() {
   return (
@@ -12,25 +13,29 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className='py-5'>
-        <h1 className='text-xl md:text-5xl text-center font-bold py-10'>
+      <main className="py-5">
+        <h1 className="text-xl md:text-5xl text-center font-bold py-10">
           Employe Management
         </h1>
-        <div className='container mx-auto flex justify-between py-5 border-b'>
-          <div className='left flex gap-3'>
-            <button className='flex  bg-gray-50  text-gray-800 px-4 py-2 border rounded-md hover:bg-indigo-500 hover:border-indigo-500 hover:text-white' >
-              Add Employee 
-              <span className='px-1'>
-              <BiUserPlus size={25}></BiUserPlus>
+        <div className="container mx-auto flex justify-between py-5 border-b">
+          <div className="left flex gap-3">
+            <button className="flex  bg-gray-50  text-gray-800 px-4 py-2 border rounded-md hover:bg-indigo-500 hover:border-indigo-500 hover:text-white">
+              Add Employee
+              <span className="px-1">
+                <BiUserPlus size={25}></BiUserPlus>
               </span>
             </button>
           </div>
         </div>
-        
+
         <div className="container mx-auto">
-        <Table />
+          <Form />
+        </div>
+
+        <div className="container mx-auto">
+          <Table />
         </div>
       </main>
     </section>
-  )
+  );
 }
