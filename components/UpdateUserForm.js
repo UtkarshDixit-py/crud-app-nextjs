337f2b4159166575b1a9a2092a62d6ada212b571
UtkarshDixit-py <utkarshdixit303@gmail.com>
2022-08-09 14:04:24 +0530

    Toggle form

---
+++ components/UpdateUserForm.js
@@ -0,0 +1,103 @@
+import { useReducer } from "react";
+import Success from "./Success";
+import Bug  from "./Bug";
+
+const formReducer = (state, event) => {
+  return {
+    ...state,
+    [event.target.name]: event.target.value,
+  };
+};
+
+export default function UpdateUserForm() {
+  const [formData, setFormData] = useReducer(formReducer, {});
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    if(Object.keys(formData).length==0)return console.log("Dont have enough data")
+    console.log(formData);
+  }
+
+  // if(Object.keys(formData).length>0) return<Success msg={"Data Added"}></Success>
+  return (
+    <form className="grid lg:grid-cols-2 w-4/6 gap-4" onSubmit={handleSubmit}>
+      <div className="input-type">
+        <input
+          onChange={setFormData}
+          type="text"
+          name="firstname"
+          className="border bg-white w-full px-5 py-3 focus:outline-none rounded-md"
+          placeholder="FirstName"
+        />
+      </div>
+      <div className="input-type">
+        <input
+          onChange={setFormData}
+          type="text"
+          name="firstname"
+          className="border bg-white w-full px-5 py-3 focus:outline-none rounded-md"
+          placeholder="LastName"
+        />
+      </div>
+      <div className="input-type">
+        <input
+          onChange={setFormData}
+          type="text"
+          name="email"
+          className="border bg-white w-full px-5 py-3 focus:outline-none rounded-md"
+          placeholder="Email"
+        />
+      </div>
+      <div className="input-type">
+        <input
+          onChange={setFormData}
+          type="text"
+          name="salary"
+          className="border bg-white w-full px-5 py-3 focus:outline-none rounded-md"
+          placeholder="Salary"
+        />
+      </div>
+      <div className="input-type">
+        <input
+          onChange={setFormData}
+          type="date"
+          name="date"
+          className="border px-5 py-3 bg-white focus:outline-none rounded-md"
+        />
+      </div>
+
+      <div className="flex gap-10 items-center">
+        <div className="form-check">
+          <input
+            onChange={setFormData}
+            type="radio"
+            value="Active"
+            id="radioDefault1"
+            name="status"
+            className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300  bg-white checked:bg-green-500 checked:border-green-500 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+          />
+          <label htmlFor="radioDefault1" className="inline-block tet-gray-800">
+            Active
+          </label>
+        </div>
+        <div className="form-check">
+          <input
+            onChange={setFormData}
+            type="radio"
+            value="Inactive"
+            id="radioDefault2"
+            name="status"
+            className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300  bg-white checked:bg-green-500 checked:border-green-500 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+          />
+          <label htmlFor="radioDefault2" className="inline-block tet-gray-800">
+            Inactive
+          </label>
+        </div>
+      </div>
+
+      <button className="flex justify-center text-md w-2/6    bg-gray-50 border-green-500 text-green-500 px-4 py-2 border rounded-md hover:bg-green-500  hover:text-white">
+        Update
+      </button>
+    </form>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,11 @@ import Table from "../components/Table";
 import Form from "../components/Form";
 
 import { BiUserPlus } from "react-icons/bi";
+import { useState } from "react";
 
 export default function Home() {
+  const [visible, setVisible] = useState(false);
+
   return (
     <section>
       <Head>
@@ -19,7 +22,10 @@ export default function Home() {
         </h1>
         <div className="container mx-auto flex justify-between py-5 border-b">
           <div className="left flex gap-3">
-            <button className="flex  bg-gray-50  text-gray-800 px-4 py-2 border rounded-md hover:bg-indigo-500 hover:border-indigo-500 hover:text-white">
+            <button
+              onClick={() => setVisible(!visible)}
+              className="flex  bg-gray-50  text-gray-800 px-4 py-2 border rounded-md hover:bg-indigo-500 hover:border-indigo-500 hover:text-white"
+            >
               Add Employee
               <span className="px-1">
                 <BiUserPlus size={25}></BiUserPlus>
@@ -27,11 +33,7 @@ export default function Home() {
             </button>
           </div>
         </div>
-
-        <div className="container mx-auto">
-          <Form />
-        </div>
-
+        {visible ? <Form /> : <></>}
         <div className="container mx-auto">
           <Table />
         </div>
